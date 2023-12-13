875575dcd263948f1a3ac6c0965bde810c33a94e
vinodkumar <kumarvinod.paleti@gmail.com>
2023-12-13 09:14:53 +0530

    files are changed

--- variables.tf
+++ variables.tf
@@ -13,3 +13,13 @@ variable "common_tags" {
     Terraform = true
   }
 }
+variable "public_subnet_cidr" {
+  default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+variable "database_subnet_cidr" {
+  default = ["10.0.21.0/24","10.0.22.0/24"]
+}
